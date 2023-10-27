0e3211a3f0b261bb1f62f215d224a7dfea8cd1f8
fvucemilo <fvucemilo@tvz.hr>
2023-10-28 01:04:11 +0200

    Add Api Gateway With Spring Cloud Gateway

--- apigw/src/main/java/com/fvucemilo/apigw/ApiGWApplication.java
+++ apigw/src/main/java/com/fvucemilo/apigw/ApiGWApplication.java
@@ -1,4 +1,4 @@
-package com.fvucemilo.notification;
+package com.fvucemilo.apigw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class NotificationApplication {
+public class ApiGWApplication {
     public static void main(String[] args) {
-        SpringApplication.run(NotificationApplication.class, args);
+        SpringApplication.run(ApiGWApplication.class, args);
     }
 }
--- customer/src/main/java/com/fvucemilo/customer/CustomerApplication.java
+++ customer/src/main/java/com/fvucemilo/customer/CustomerApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients(
         basePackages = "com.fvucemilo.clients"
 )
+@SpringBootApplication
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
